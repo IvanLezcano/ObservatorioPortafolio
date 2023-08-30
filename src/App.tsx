04593b3bf8ad88e0ac4a637bc51c721762fc3277
Ivan Lezcano <gabrielivanlezcano@gmail.com>
2023-08-30 01:48:48 -0300

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import Header from './Header';
 import { Link } from "react-router-dom";
 import './app.css'
+import vecinos from './KJ3PQQWQMVAHFFHHGVTQLYE67M.png'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
@@ -39,6 +40,46 @@ function App(): JSX.Element {
                       </button>
                       </div>
           </div>
+
+          <div className="row row-cols-1 row-cols-md-2 g-4">
+  <div className="col">
+    <div className="card">
+      <img src={vecinos} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card SADASDAtitle</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src={vecinos} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src={vecinos} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src={vecinos} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
   )
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -2,16 +2,60 @@ import React, { Fragment, useState } from 'react';
 import logo from './logo.svg';
 import { Link} from "react-router-dom";
 import "./Header.css"
+import {BsPerson} from "react-icons/bs"
 
 export default function Header(): JSX.Element {
 
  
 
   return (
-    
-    <div className="banner_principal">
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <BsPerson/>
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+<h1>sdasd</h1>
+
+<div className="banner_principal">
       <h1 className='titulo'>Observatorio De Registros De Ivan</h1>
     </div>
+
+    </div>
+   
     
   )
 }
